a85f3dc5b6ed487c0e62247580931b87d661cc85
axreldable <starikovaleksei1992@mail.ru>
2020-04-01 18:15:08 +0300

    fp: deploy flink jobs to the flink docker cluster properly

--- final-project/deploy_jobs.sh
+++ final-project/deploy_jobs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eu
+
 ./input-adapter/deploy.sh & \
 ./flink-tweet-job/deploy.sh & \
 ./output-adapter/deploy.sh &
--- final-project/input-adapter/deploy.sh
+++ final-project/input-adapter/deploy.sh
@@ -5,5 +5,8 @@ set -eu
 SPATH=$(dirname $0)
 
 VERSION=1.0.0
+JAR_NAME=input-adapter_${VERSION}.jar
 
-flink run ${SPATH}/target/scala-2.11/input-adapter_${VERSION}.jar
+JOBMANAGER_CONTAINER=$(docker ps --filter name=jobmanager --format={{.ID}})
+docker cp ${SPATH}/target/scala-2.11/$JAR_NAME "$JOBMANAGER_CONTAINER":/$JAR_NAME
+docker exec -t -i "$JOBMANAGER_CONTAINER" flink run /$JAR_NAME
